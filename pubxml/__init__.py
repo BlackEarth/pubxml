389349ea69d72a20b1e1e72b134cf1a003cb9e82
Sean Harrison <sah@blackearth.us>
2016-11-14 16:55:51 -0600

    Dict syntax is better

--- pubxml/__init__.py
+++ pubxml/__init__.py
@@ -1,21 +1,20 @@
 
 import os
 from bl.dict import Dict
+
 schemas = os.path.join(os.path.dirname(__file__), 'resources', 'publishing-xml', 'schemas')
 
-NS = Dict(**{
-    'pub': "http://publishingxml.org/ns",
-    'html': "http://www.w3.org/1999/xhtml",
-    # 'xl': "http://www.w3.org/1999/xlink",
-    # 'xi': "http://www.w3.org/2001/XInclude",
-    'dc': "http://purl.org/dc/elements/1.1/",                                   # Dublin Core & friends
-    'dcterms': "http://purl.org/dc/terms/", 
-    'dcmitype': "http://purl.org/dc/dcmitype/", 
-    'xsi': "http://www.w3.org/2001/XMLSchema-instance",                         # XML & friends
-    'xml': "http://www.w3.org/XML/1998/namespace",
-    'opf': "http://www.idpf.org/2007/opf",                                      # Digital Publishing
-    'container': "urn:oasis:names:tc:opendocument:xmlns:container", 
-    'epub': "http://www.idpf.org/2007/ops",
-    'ncx': "http://www.daisy.org/z3986/2005/ncx/",
-    'cp': "http://schemas.openxmlformats.org/package/2006/metadata/core-properties",    # Microsoft
-})
+NS = Dict(
+    pub="http://publishingxml.org/ns",
+    html="http://www.w3.org/1999/xhtml",
+    dc="http://purl.org/dc/elements/1.1/",                                   # Dublin Core & friends
+    dcterms="http://purl.org/dc/terms/", 
+    dcmitype="http://purl.org/dc/dcmitype/", 
+    xsi="http://www.w3.org/2001/XMLSchema-instance",                         # XML & friends
+    xml="http://www.w3.org/XML/1998/namespace",
+    opf="http://www.idpf.org/2007/opf",                                      # Digital Publishing
+    container="urn:oasis:names:tc:opendocument:xmlns:container", 
+    epub="http://www.idpf.org/2007/ops",
+    ncx="http://www.daisy.org/z3986/2005/ncx/",
+    cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties",    # Microsoft
+)
