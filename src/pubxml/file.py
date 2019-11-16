77d3964714b6ec09f9af697de9786911b8f32a38
Sean Harrison <sah@bookgenesis.com>
2019-11-16 14:18:59 -0600

    File class, provides .path and .readable_size

---
+++ src/pubxml/file.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class File:
+    filename: str
+
+    @property
+    def path(self):
+        return Path(self.filename)
+
+    @classmethod
+    def readable_size(C, size, suffix='B', decimals=1, sep='\u00a0'):
+        """
+        Given the file size in bytes, return a string with the human-readable size.
+        """
+        SIZE_UNITS = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+        if size is None:
+            return
+        size = float(size)
+        for unit in SIZE_UNITS:
+            if abs(size) < 1024 or unit == SIZE_UNITS[-1]:
+                return "{size:.{decimals}f}{sep}{unit}{suffix}".format(
+                    size=size,
+                    unit=unit,
+                    suffix=suffix,
+                    sep=sep,
+                    decimals=decimals if SIZE_UNITS.index(unit) > 0 else 0,
+                )
+            size /= 1024
